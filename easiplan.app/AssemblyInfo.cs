b011c7cbc75ab49319c3518432eeb3c23b20e68e
Yusuf Jattiem <yusuf@easiworx.co.za>
2023-07-30 19:29:19 +0000

    Updated AssemblyInfo.cs

--- easiplan.app/AssemblyInfo.cs
+++ easiplan.app/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 #elif STAGING
     [assembly: AssemblyTitle("EasiWorx Staging")]  
     [assembly: AssemblyProduct("EasiWorx Staging")]
-    [assembly: AssemblyVersion("1.0.87")] 
-    [assembly: AssemblyFileVersion("1.0.87")] 
+    [assembly: AssemblyVersion("1.0.88")] 
+    [assembly: AssemblyFileVersion("1.0.88")] 
 #elif MOJAFF
     [assembly: AssemblyTitle("EasiWorx Mojaff Preview")]
     [assembly: AssemblyProduct("EasiWorx Mojaff")]
